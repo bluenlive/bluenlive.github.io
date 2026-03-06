c34bdb10a699db81c89f818856ac53a57a37036b
BLUEnLIVE <bluenlive@gmail.com>
2026-03-06 13:58:31 +0900

    test8

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -14,34 +14,37 @@ var store = [
       {%- else -%}
         {%- assign teaser = site.teaser -%}
       {%- endif -%}
-      {
-        {% comment %} 1. 제목은 원본 그대로 표시합니다 {% endcomment %}
-        "title": {{ doc.title | jsonify }},
 
-        {% comment %} 2. 요약문에서 특수문자를 제거하여 '파묘' 등을 검색 가능하게 만듭니다 {% endcomment %}
+      {%- comment %} 1. 검색 엔진을 위한 '세탁된 제목' 생성 {% endcomment -%}
+      {%- assign search_keywords = doc.title | 
+        replace: '〈', ' ' | replace: '〉', ' ' | replace: '《', ' ' | replace: '》', ' ' | 
+        replace: '(', ' ' | replace: ')', ' ' | replace: '[', ' ' | replace: ']', ' ' | 
+        replace: '【', ' ' | replace: '】', ' ' | replace: ',', ' ' | replace: '.', ' ' | 
+        replace: ':', ' ' | replace: '꞉', ' ' | replace: '/', ' ' -%}
+
+      {
+        {% comment %} 2. 화면 표시용: 원본 제목 그대로 유지 {% endcomment %}
+        "title": {{ doc.title | jsonify }},
+        
+        {% comment %} 3. 요약문: 본문을 먼저 보여주고, 검색용 키워드는 맨 뒤에 추가 {% endcomment %}
         "excerpt":
-          {%- if site.search_full_content == true -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " | replace:"</p>", " " | replace:"</h1>", " " |
-              replace:"</h2>", " " | replace:"</h3>", " " | replace:"</h4>", " " |
-              replace:"</h5>", " " | replace:"</h6>", " "|
-              strip_html | strip_newlines | 
-              replace: '〈', ' ' | replace: '〉', ' ' | replace: '《', ' ' | replace: '》', ' ' | 
-              replace: '(', ' ' | replace: ')', ' ' | replace: '[', ' ' | replace: ']', ' ' | 
-              replace: '【', ' ' | replace: '】', ' ' | replace: ',', ' ' | replace: '.', ' ' | 
-              replace: ':', ' ' | replace: '꞉', ' ' | replace: '/', ' ' | jsonify }},
-          {%- else -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " | replace:"</p>", " " | replace:"</h1>", " " |
-              replace:"</h2>", " " | replace:"</h3>", " " | replace:"</h4>", " " |
-              replace:"</h5>", " " | replace:"</h6>", " "|
-              strip_html | strip_newlines | 
-              replace: '〈', ' ' | replace: '〉', ' ' | replace: '《', ' ' | replace: '》', ' ' | 
-              replace: '(', ' ' | replace: ')', ' ' | replace: '[', ' ' | replace: ']', ' ' | 
-              replace: '【', ' ' | replace: '】', ' ' | replace: ',', ' ' | replace: '.', ' ' | 
-              replace: ':', ' ' | replace: '꞉', ' ' | replace: '/', ' ' | 
-              truncatewords: 50 | jsonify }},
-          {%- endif -%}
+          {%- capture combined_excerpt -%}
+            {%- if site.search_full_content == true -%}
+              {{ doc.content | newline_to_br |
+                replace:"<br />", " " | replace:"</p>", " " | replace:"</h1>", " " |
+                replace:"</h2>", " " | replace:"</h3>", " " | replace:"</h4>", " " |
+                replace:"</h5>", " " | replace:"</h6>", " "|
+                strip_html | strip_newlines }} {{ search_keywords }}
+            {%- else -%}
+              {{ doc.content | newline_to_br |
+                replace:"<br />", " " | replace:"</p>", " " | replace:"</h1>", " " |
+                replace:"</h2>", " " | replace:"</h3>", " " | replace:"</h4>", " " |
+                replace:"</h5>", " " | replace:"</h6>", " "|
+                strip_html | strip_newlines | truncatewords: 50 }} {{ search_keywords }}
+            {%- endif -%}
+          {%- endcapture -%}
+          {{ combined_excerpt | jsonify }},
+
         "categories": {{ doc.categories | jsonify }},
         "tags": {{ doc.tags | jsonify }},
         "url": {{ doc.url | relative_url | jsonify }},
@@ -54,25 +57,28 @@ var store = [
     {%- if forloop.last -%}
       {%- assign l = true -%}
     {%- endif -%}
+    
+    {%- assign page_keywords = doc.title | replace: '〈', ' ' | replace: '〉', ' ' | replace: '꞉', ' ' | replace: ':', ' ' -%}
+
   {
     "title": {{ doc.title | jsonify }},
     "excerpt":
+      {%- capture page_excerpt -%}
         {%- if site.search_full_content == true -%}
           {{ doc.content | newline_to_br |
             replace:"<br />", " " | replace:"</p>", " " | replace:"</h1>", " " |
             replace:"</h2>", " " | replace:"</h3>", " " | replace:"</h4>", " " |
             replace:"</h5>", " " | replace:"</h6>", " "|
-            strip_html | strip_newlines | 
-            replace: '〈', ' ' | replace: '〉', ' ' | replace: '꞉', ' ' | replace: ':', ' ' | jsonify }},
+            strip_html | strip_newlines }} {{ page_keywords }}
         {%- else -%}
           {{ doc.content | newline_to_br |
             replace:"<br />", " " | replace:"</p>", " " | replace:"</h1>", " " |
             replace:"</h2>", " " | replace:"</h3>", " " | replace:"</h4>", " " |
             replace:"</h5>", " " | replace:"</h6>", " "|
-            strip_html | strip_newlines | 
-            replace: '〈', ' ' | replace: '〉', ' ' | replace: '꞉', ' ' | replace: ':', ' ' | 
-            truncatewords: 50 | jsonify }},
-        {%- endif -%},
+            strip_html | strip_newlines | truncatewords: 50 }} {{ page_keywords }}
+        {%- endif -%}
+      {%- endcapture -%}
+      {{ page_excerpt | jsonify }},
     "url": {{ doc.url | absolute_url | jsonify }}
   }{%- unless forloop.last and l -%},{%- endunless -%}
   {%- endfor -%}
